6bad7d491d34a78b297487d195ef54cc82397ce7
tinnan <tin_nan@hotmail.com>
2024-06-03 10:20:04 +0700

    Add log message.

--- learn-mongodb-csfle/src/main/java/com/mongodb/tutorials/csfle/ClientSideFieldLevelEncryptionTutorial.java
+++ learn-mongodb-csfle/src/main/java/com/mongodb/tutorials/csfle/ClientSideFieldLevelEncryptionTutorial.java
@@ -76,14 +76,15 @@ public class ClientSideFieldLevelEncryptionTutorial {
             MongoDatabase database = client.getDatabase(encryptedDatabaseName);
             MongoCollection<Document> collection = database.getCollection(encryptedCollectionName);
 
-//            deterministicVsRandomize(clientEncryption, collection, ssnDataKey, billingDataKey);
+            deterministicVsRandomize(clientEncryption, collection, ssnDataKey, billingDataKey);
 //            encryptDecrypt(clientEncryption, collection, ssnDataKey, billingDataKey);
-            sortBySsn(clientEncryption, collection, ssnDataKey, billingDataKey);
+//            sortBySsn(clientEncryption, collection, ssnDataKey, billingDataKey);
         }
     }
 
     private static void sortBySsn(ClientEncryption clientEncryption,
         MongoCollection<Document> collection, BsonBinary ssnDataKey, BsonBinary billingDataKey) {
+        log.info("***** Sort by SSN *****");
 
         final String cardType = "Visa";
         final String cardNumber = "4111111111111111";
@@ -100,20 +101,21 @@ public class ClientSideFieldLevelEncryptionTutorial {
             cardNumber);
         insert(collection, document3);
 
-        // !!! Sort by encrypted field cannot give correct result.
         try (MongoCursor<Document> sorted = collection.find().sort(Sorts.ascending("patientRecord.ssn")).iterator()) {
             while (sorted.hasNext()) {
                 Document doc = sorted.next();
                 Document patientRecord = doc.get("patientRecord", Document.class);
                 Binary ssnField = patientRecord.get("ssn", Binary.class);
                 BsonValue decrypted = clientEncryption.decrypt(new BsonBinary(ssnField.getType(), ssnField.getData()));
-                log.info("Sorted Decrypted: {}", decrypted);
+                log.info("Sorted: {}", decrypted);
             }
         }
+        log.info("* !!! Sort by encrypted field cannot give correct result.");
     }
 
     private static void encryptDecrypt(ClientEncryption clientEncryption,
         MongoCollection<Document> collection, BsonBinary ssnDataKey, BsonBinary billingDataKey) {
+        log.info("***** Encrypt / Decrypt *****");
 
         final String patientName = "Jon Doe";
         final String ssn = "987-65-4320";
@@ -139,15 +141,7 @@ public class ClientSideFieldLevelEncryptionTutorial {
 
         Binary billingField = patientRecord.get("billing", Binary.class);
         patientRecord.replace("billing", clientEncryption.decrypt(new BsonBinary(billingField.getType(), billingField.getData())));
-        log.info("Encrypted Document: {}", result.toJson());
-    }
-
-    private static BsonBinary createKey(ClientEncryption clientEncryption, String keyAltName) {
-        BsonDocument dataKeyDoc = clientEncryption.getKeyByAltName(keyAltName);
-        if (dataKeyDoc != null) {
-            return dataKeyDoc.getBinary("_id");
-        }
-        return clientEncryption.createDataKey(KMS_PROVIDER_NAME, new DataKeyOptions().keyAltNames(List.of(keyAltName)));
+        log.info("Decrypted Document: {}", result.toJson());
     }
 
     private static void deterministicVsRandomize(ClientEncryption clientEncryption,
@@ -155,6 +149,10 @@ public class ClientSideFieldLevelEncryptionTutorial {
         // Insert same data for 2 records.
         // - patientRecord.ssn field should store same binary result.
         // - patientRecord.billing field should store different binary result.
+        log.info("***** Deterministic vs Randomized encryption *****");
+        log.info("* Insert same data for 2 records");
+        log.info("* - encrypted patientRecord.ssn field should store same binary result");
+        log.info("* - encrypted patientRecord.billing field should store different binary result");
 
         final String patientName = "Jon Doe";
         final String ssn = "987-65-4320";
@@ -172,13 +170,17 @@ public class ClientSideFieldLevelEncryptionTutorial {
 
         try (MongoCursor<Document> sorted = collection.find().iterator()) {
             while (sorted.hasNext()) {
-                Document doc = sorted.next();
-                Document patientRecord = doc.get("patientRecord", Document.class);
-                Binary ssnField = patientRecord.get("ssn", Binary.class);
-                BsonValue decrypted = clientEncryption.decrypt(new BsonBinary(ssnField.getType(), ssnField.getData()));
-                log.info("Sorted Decrypted: {}", decrypted);
+                log.info("{}", sorted.next().toJson());
             }
         }
+    }
+
+    private static BsonBinary createKey(ClientEncryption clientEncryption, String keyAltName) {
+        BsonDocument dataKeyDoc = clientEncryption.getKeyByAltName(keyAltName);
+        if (dataKeyDoc != null) {
+            return dataKeyDoc.getBinary("_id");
+        }
+        return clientEncryption.createDataKey(KMS_PROVIDER_NAME, new DataKeyOptions().keyAltNames(List.of(keyAltName)));
     }
 
     private static Document createDocument(ClientEncryption clientEncryption, BsonBinary ssnDataKey, BsonBinary billingDataKey,
