d83e6879beb41a1ce53958029377091234fac186
tinnan <tin_nan@hotmail.com>
2024-06-11 08:08:08 +0700

    Example for data driven.

--- gradle-spock-demo/src/test/groovy/com/example/demo/datadriven/DataDrivenSpec.groovy
+++ gradle-spock-demo/src/test/groovy/com/example/demo/datadriven/DataDrivenSpec.groovy
@@ -13,4 +13,14 @@ class DataDrivenSpec extends Specification {
         7 | 3 | 7
         5 | 5 | 5
     }
+
+    def "Math.max with data table input"() {
+        expect:
+        Math.max(a, b) == c;
+
+        where:
+        a << [2, 7, 5]
+        b << [5, 3, 5]
+        c << [5, 7, 5]
+    }
 }
