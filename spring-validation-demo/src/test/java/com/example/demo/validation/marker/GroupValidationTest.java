a57a78ebc6fe2a3312ec01cd57ff13ab72b6dbfa
tinnan <tin_nan@hotmail.com>
2024-07-04 00:49:14 +0700

    Add test to clarify how validation group works.

---
+++ spring-validation-demo/src/test/java/com/example/demo/validation/marker/GroupValidationTest.java
@@ -0,0 +1,99 @@
+package com.example.demo.validation.marker;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import jakarta.validation.constraints.NotBlank;
+import java.util.Set;
+import lombok.Data;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class GroupValidationTest {
+    private static Validator validator;
+
+    @BeforeAll
+    public static void beforeAll() {
+        try (ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory()) {
+            validator = validatorFactory.getValidator();
+        }
+    }
+
+    @Test
+    void givenAllUserGroup_whenValidate_shouldValidateFieldWithAnnotationWithGroupAllUser() {
+        User user = new User();
+
+        Set<ConstraintViolation<User>> violations = validator.validate(user, AllUser.class);
+        assertEquals(1, violations.size());
+
+        assertViolation(violations, "name", "must not be blank");
+    }
+
+    @Test
+    void givenRegularUserGroup_whenValidate_shouldValidateFieldWithAnnotationWithGroupRegularUser() {
+        User user = new User();
+
+        Set<ConstraintViolation<User>> violations = validator.validate(user, RegularUser.class);
+        assertEquals(2, violations.size());
+
+        assertViolation(violations, "email", "must not be blank");
+        assertViolation(violations, "username", "must not be blank");
+    }
+
+    @Test
+    void givenRegularUserAndAdminUserGroup_whenValidate_shouldValidateFieldWithAnnotationWithGroupRegularUserAndAdminUser() {
+        User user = new User();
+
+        Set<ConstraintViolation<User>> violations = validator.validate(user, RegularUser.class, AdminUser.class);
+        assertEquals(2, violations.size());
+
+        assertViolation(violations, "username", "must not be blank");
+    }
+
+    @Test
+    void givenOtherUserGroup_whenValidate_shouldNotValidateAnyField() {
+        User user = new User();
+
+        Set<ConstraintViolation<User>> violations = validator.validate(user, OtherUser.class);
+        assertEquals(0, violations.size());
+    }
+
+    @Test
+    void givenOtherUserAndRegularUserGroup_whenValidate_shouldValidateFieldWithAnnotationWithGroupRegularUser() {
+        User user = new User();
+
+        Set<ConstraintViolation<User>> violations = validator.validate(user, OtherUser.class, RegularUser.class);
+        assertEquals(2, violations.size());
+
+        assertViolation(violations, "email", "must not be blank");
+        assertViolation(violations, "username", "must not be blank");
+    }
+
+    private void assertViolation(Set<ConstraintViolation<User>> violations, String expectedField,
+        String expectedMessage) {
+        for (ConstraintViolation<User> action : violations) {
+            if (expectedField.equals(action.getPropertyPath()
+                .toString())) {
+                assertEquals(expectedMessage, action.getMessage());
+                return;
+            }
+        }
+        fail("Expected field [" + expectedField + "] to be in violation list.");
+    }
+
+    @Data
+    private static class User {
+        @NotBlank(groups = AllUser.class)
+        private String name;
+
+        @NotBlank(groups = RegularUser.class)
+        private String email;
+
+        @NotBlank(groups = {RegularUser.class, AdminUser.class})
+        private String username;
+    }
+}
