17f9a6090dd18024c77d44e8a10e5c8346c3ca74
tinnan <tin_nan@hotmail.com>
2024-09-12 17:09:41 +0700

    remove unused

--- learn-spring-web/src/main/java/com/example/demo/feignconfig/clients/ServerClient.java
+++ learn-spring-web/src/main/java/com/example/demo/feignconfig/clients/ServerClient.java
@@ -1,12 +1,10 @@
 package com.example.demo.feignconfig.clients;
 
-import feign.RequestInterceptor;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient(name = "server-client", url = "${feign.server-client.url}",
-    configuration = {RequestInterceptor.class})
+@FeignClient(name = "server-client", url = "${feign.server-client.url}")
 public interface ServerClient {
 
     @GetMapping("/server")
