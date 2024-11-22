62f65b2b68078a041b8bf2eac36993c2aae38e99
tinnan <tin_nan@hotmail.com>
2024-11-22 09:48:48 +0700

    add log message

--- learn-spring-web/src/main/java/com/example/demo/requestscope/config/context/ContextAwareCallable.java
+++ learn-spring-web/src/main/java/com/example/demo/requestscope/config/context/ContextAwareCallable.java
@@ -1,9 +1,11 @@
 package com.example.demo.requestscope.config.context;
 
 import java.util.concurrent.Callable;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
 
+@Slf4j
 public class ContextAwareCallable<T> implements Callable<T> {
 
     private final CustomRequestScopeAttr requestAttributes;
@@ -17,6 +19,7 @@ public class ContextAwareCallable<T> implements Callable<T> {
     @Override
     public T call() throws Exception {
         try {
+            log.info("ContextAwareCallable - call");
             RequestContextHolder.setRequestAttributes(requestAttributes);
             return task.call();
         } finally {
--- learn-spring-web/src/main/java/com/example/demo/requestscope/config/context/ContextAwareRunnable.java
+++ learn-spring-web/src/main/java/com/example/demo/requestscope/config/context/ContextAwareRunnable.java
@@ -1,21 +1,24 @@
 package com.example.demo.requestscope.config.context;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
 
+@Slf4j
 public class ContextAwareRunnable implements Runnable {
 
     private final CustomRequestScopeAttr requestAttributes;
     private final Runnable task;
 
     public ContextAwareRunnable(RequestAttributes requestAttributes, Runnable task) {
-        this.task = task;
         this.requestAttributes = ContextUtils.cloneRequestAttributes(requestAttributes);
+        this.task = task;
     }
 
     @Override
     public void run() {
         try {
+            log.info("ContextAwareRunnable - run");
             RequestContextHolder.setRequestAttributes(requestAttributes);
             task.run();
         } finally {
